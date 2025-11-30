86b36c796d8f541fd553da94aea3bd905e2597f1
juliaku645 <iuliakurdyuk@gmail.com>
2025-11-30 15:35:45 +0300

    Работает функция описание, добавление контекста. Есть возможность повторного обращения к боту.

--- app/database.py
+++ app/database.py
@@ -16,24 +16,24 @@ class Database:
     async def close(self):
         await self.db.close()
 
-    async def save_or_update_image(self, user_id: int, image_bytes: bytes,context: str=None):
+    async def save_or_update_image(self, user_id: int, image_bytes: bytes,context_text: str=None):
         async with self.db_lock:
             async with self.db.execute("SELECT 1 FROM images WHERE user_id=?", (user_id,)) as cursor:
                 exists = await cursor.fetchone()
             if exists:
-                await self.db.execute("UPDATE images SET photo=?, context=? WHERE user_id=?", (image_bytes, context, user_id))
+                await self.db.execute("UPDATE images SET photo=?, context=? WHERE user_id=?", (image_bytes, context_text, user_id))
             else:
-                await self.db.execute("INSERT INTO images (user_id,  photo, context) VALUES (?, ?, ?)", (user_id, image_bytes, context))
+                await self.db.execute("INSERT INTO images (user_id,  photo, context) VALUES (?, ?, ?)", (user_id, image_bytes, context_text))
             await self.db.commit()
-    async def update_text(self, user_id: int, context: str):
+    async def update_text(self, user_id: int, context_text: str):
         async with self.db_lock:
             async with self.db.execute("SELECT 1 FROM images WHERE user_id=?", (user_id,)) as cursor:
                 exists = await cursor.fetchone()
             if exists:
-                await self.db.execute("UPDATE images SET context=? WHERE user_id=?",(context,user_id))
+                await self.db.execute("UPDATE images SET context=? WHERE user_id=?",(context_text,user_id))
             else:
                 await self.db.execute("INSERT INTO images (user_id, context) VALUES (?, ?)",
-                                      (user_id, context))
+                                      (user_id, context_text))
             await self.db.commit()
             print("Текст сохранен в БД")
 
@@ -60,9 +60,9 @@ class Database:
                     return row[0]
                 return None
 
-    async def save_description(self, user_id: int, context: str, image_bytes: bytes,description: str):
+    async def save_description(self, user_id: int, context_text: str, image_bytes: bytes,description: str):
         async with self.db_lock:
-            async with self.db.execute("INSERT INTO LLM_replies (user_id, context, photo,description) VALUES (?, ?, ?, ?)", (user_id, context,image_bytes, description)):
+            async with self.db.execute("INSERT INTO LLM_replies (user_id, context, photo,description) VALUES (?, ?, ?, ?)", (user_id, context_text,image_bytes, description)):
                 await self.db.commit()
 
 
